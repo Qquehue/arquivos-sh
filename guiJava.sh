9c5cfa1eaa07030568e67475ffe34e1514e1bd3c
Qquehue <alejandro.qquehue@sptech.school>
2022-11-29 14:33:01 -0300

    cira bd

--- guiJava.sh
+++ guiJava.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+cd /home/ubuntu
 git clone https://github.com/Qquehue/Console-Tech-Consulting.git
-cd Console-Tech-Consulting/API/local-app/target
 
-appInit(){
-cd /home/ubuntu/Desktop/Web.App/Console-Tech-Consulting/API/local-app/target
-java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
-}
+#appInit(){
+#cd /home/ubuntu/Desktop/Web.App/Console-Tech-Consulting/API/local-app/target
+#java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
+#}
+cd /home/ubuntu/Script-CTC
+sudo systemctl start docker
+  sudo systemctl enable docker
 
+docker build -t dockerfile .
+docker run -d -p 3306:3306 --name ctc -e MYSQL_ROOT_PASSWORD=urubu100 -e MYSQL_DATABASE=ctc dockerfile
+
+cd /home/ubuntu/Console-Tech-Consulting/API/local-app/target
 #sudo docker build -t dockerfile .
 #sudo docker run -d -t --name ctc-cli dockerfile
 sudo chmod 777 local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
