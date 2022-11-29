7efddba6d3c3954c7ec5b71b9f947888f37e8dc5
Qquehue <alejandro.qquehue@sptech.school>
2022-11-29 19:33:39 -0300

    users e private

--- guiJava.sh
+++ guiJava.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-git clone https://github.com/Qquehue/Console-Tech-Consulting.git
+git clone https://Qquehue@github.com/Qquehue/Console-Tech-Consulting
 #appInit(){
 #cd /home/ubuntu/Desktop/Web.App/Console-Tech-Consulting/API/local-app/target
 #java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 #}
-
 cd Console-Tech-Consulting/API/local-app/target
 #sudo docker build -t dockerfile .
 #sudo docker run -d -t --name ctc-cli dockerfile
