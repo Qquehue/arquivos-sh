0558a2f500dacdb3c8e5ccafa568c4cc6fe8b4b4
Qquehue <alejandro.qquehue@sptech.school>
2022-11-29 10:02:57 -0300

    caminho

--- guiJava.sh
+++ guiJava.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/Desktop
 mkdir Local.App
 cd Local.App
 git clone https://github.com/Qquehue/Console-Tech-Consulting.git
-cd /home/ubuntu/Desktop/Local.App/Console-Tech-Consulting/API/local-app/target
+cd Console-Tech-Consulting/API/local-app/target
 xauth add $(xauth -f /home/ubuntu/.Xauthority list|tail -l)
 export DISPLAY=:0.0
 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
