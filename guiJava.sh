50e4b26616129fe9ee2d4c339773bb008058cfe0
Qquehue <alejandro.qquehue@sptech.school>
2022-11-29 09:28:01 -0300

    tentativa root

--- guiJava.sh
+++ guiJava.sh
@@ -4,14 +4,7 @@ cd /home/ubuntu/Desktop
 mkdir Local.App
 cd Local.App
 git clone https://github.com/Qquehue/Console-Tech-Consulting.git
-cd Console-Tech-Consulting/API/local-app/target
-
-#appInit(){
-#cd /home/ubuntu/Desktop/Web.App/Console-Tech-Consulting/API/local-app/target
-#java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
-#}
-
-sudo chmod 777 local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 xauth add $(xauth -f /home/ubuntu/.Xauthority list|tail -l)
 export DISPLAY=:0.0
+cd /home/ubuntu/Desktop/Web.App/Console-Tech-Consulting/API/local-app/target
 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
