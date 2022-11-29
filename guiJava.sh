f4fe485913f87d744aceef26299d1d5283d9a2a2
Qquehue <alejandro.qquehue@sptech.school>
2022-11-29 01:15:25 -0300

    ajuste misc

--- guiJava.sh
+++ guiJava.sh
@@ -6,8 +6,6 @@ cd Local.App
 git clone https://github.com/Qquehue/Console-Tech-Consulting.git
 cd Console-Tech-Consulting/API/local-app/target
 
-<<<<<<< HEAD
-
 #sudo chmod 777 local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 
@@ -15,7 +13,6 @@ java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 #cd /home/ubuntu/Desktop/Web.App/Console-Tech-Consulting/API/local-app/target
 #java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 #}
-=======
   sudo systemctl start docker
   sudo systemctl enable docker
 sudo docker exec -it ctcBD mysql -uroot -p
@@ -24,4 +21,5 @@ mysql> ALTER USER 'root'@'localhost' IDENTIFIED BY 'urubu100';
 
 sudo chmod 777 local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
->>>>>>> 7ecddd3df247266c79852634347d24321d777099
+xauth add $(xauth -f /home/ubuntu/.Xauthority list|tail -l)
+export DISPLAY=:0.0
