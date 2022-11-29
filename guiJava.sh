46d796c52f227fae9a237dc60d24933acd9c74da
Qquehue <alejandro.qquehue@sptech.school>
2022-11-29 11:46:07 -0300

    chmod

--- guiJava.sh
+++ guiJava.sh
@@ -5,5 +5,5 @@ git clone https://github.com/Qquehue/Console-Tech-Consulting.git
 cd Console-Tech-Consulting/API/local-app/target
 #xauth add $(xauth -f /home/ubuntu/.Xauthority list|tail -l)
 #export DISPLAY=:0.0
-sudo chomod 777 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
+sudo chmod 777 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
