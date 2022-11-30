35c7fc7f545a9346508b9092a465f59c1a732d2e
Qquehue <alejandro.qquehue@sptech.school>
2022-11-29 22:10:38 -0300

    nova

--- guiJava.sh
+++ guiJava.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://Qquehue@github.com/Qquehue/Console-Tech-Consulting
-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
+#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
 cd /home/ubuntu/Script-CTC
 sudo systemctl start docker
 sudo systemctl enable docker
@@ -12,7 +12,5 @@ sudo docker run -d -p 3306:3306 --name bd-CTC -e MYSQL_ROOT_PASSWORD=urubu100 -e
 #java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 #}
 cd /home/ubuntu/arquivos-sh/Console-Tech-Consulting/API/local-app/target
-#sudo docker build -t dockerfile .
-#sudo docker run -d -t --name ctc-cli dockerfile
 sudo chmod 777 local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
 java -jar local-app-1.0-SNAPSHOT-jar-with-dependencies.jar
